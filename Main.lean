706f25804db957366807e557df2f81aaae0aea07
Yuichi Komorida <nicomathajax32@gmail.com>
2026-01-07 21:13:36 +0900

    Add template mainly for IO

--- Main.lean
+++ Main.lean
@@ -1,2 +1,8 @@
-def main : IO Unit :=
-  IO.println s!"Hello, world!"
+-- Template
+def main : IO Unit := do
+  let stdin ← IO.getStdin
+  let instr ← stdin.readToEnd
+  let intokens := instr.split (·.isWhitespace)
+  let a := intokens[0]!.trim.toNat!
+  let b := intokens[1]!.trim.toNat!
+  IO.println "Yeah"
